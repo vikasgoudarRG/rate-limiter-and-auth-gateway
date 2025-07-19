237004da1dc3fa0af5d3d308b406b674559492a2
vikasgoudarRG <vikasgoudar123@gmail.com>
2025-07-19 23:21:57 +0530

    created maven

---
+++ backend/rate-limiter/src/test/java/dev/vikas/rate_limiter/RateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vikas.rate_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
